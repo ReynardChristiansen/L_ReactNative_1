bdc240d2802b8bc8c0270c29c24ca44c431d0fad
ReynardChristiansen <reynard.satria@gmail.com>
2023-09-22 19:40:40 +0700

    learning ReactNative with simple project using state and fetch api

--- App.js
+++ App.js
@@ -1,20 +1,100 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {View, Text,TextInput, Button, StyleSheet, ScrollView, FlatList, Pressable} from 'react-native'
+import {useState} from "react";
+import ApiCall from './components';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+export default function App(){
+  const [value, setValue] = useState('');
+  const [listOfNotes, setListOfNotes] = useState([]);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  function handleOnChangeText(getEnteredText){
+    setValue(getEnteredText)
+  }
+
+  function handleOnPressButton(){
+    if (value.trim() !== '') {
+      setListOfNotes(currentNotes => [...currentNotes, value]);
+      setValue(''); 
+      setErrorMessage('');
+    } 
+    else {
+      setErrorMessage('Cannot NULL');
+    }
+  }
+
+  function handleRemoveItem(getCurrentIndex){
+      let cpyListOfNotes = [...listOfNotes]
+      cpyListOfNotes = cpyListOfNotes.filter((_,index)=> {
+        if(getCurrentIndex !== index){
+          return true;
+        }
+        else{
+          return false;
+        }
+      });
+        
+      setListOfNotes(cpyListOfNotes)
+  }
+
+
+  return(
+    <View style={{paddingTop:50, marginLeft:20, marginRight:20}}>
+        <View style={styles.inputContainer}>
+          <TextInput onChangeText={handleOnChangeText} value={value} style={styles.input} placeholder="Add Your Note Here"></TextInput>
+          <Button onPress={handleOnPressButton} color={'#000'} title="Add Noted"/>
+        </View> 
+
+        {errorMessage !== '' && (
+        <Text>{errorMessage}</Text>
+        )}
+
+        <View style={styles.listContainer}>
+
+          <FlatList data={listOfNotes} renderItem={(itemData)=>(
+            <Pressable onPress={()=> handleRemoveItem(itemData.index)}>
+              <Text style={styles.listItem}>{itemData.item}</Text>
+            </Pressable>
+            
+          )}/>
+          {/* {
+            listOfNotes.map((item,index)=>  <Text style={styles.listItem} key={`item${index}`}>{item}</Text>)
+          } */}
+
+        </View>
+
+        <View>
+          <ApiCall/>
+        </View>
+
+        
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  inputContainer :{
+    flexDirection : 'row',
+    paddingBottom:30,
+    borderBottomWidth:1,
   },
-});
+  input:{
+    borderWidth:1,
+    borderColor:'#ddd',
+    flex: 1
+  },
+  listContainer:{
+    paddingTop:30,
+  },
+  listItem:{
+    borderRadius:1,
+    borderColor:'red',
+    backgroundColor:'green',
+    paddingBottom:20,
+    marginBottom:20,
+    color: 'white',
+    fontSize: 15
+  },
+})
+
+
---
+++ components/index.jsx
@@ -0,0 +1,44 @@
+import { useEffect } from 'react'
+import {View, Text, FlatList, ActivityIndicator} from 'react-native'
+import {useState} from "react";
+
+export default function ApiCall(){
+    const [loading, setLoading]= useState(false)
+
+
+    const [apiData, setApiData] = useState([])
+
+    useEffect(()=>{
+        setLoading(true)
+
+        async function getDataFromApi(){
+            const apiResponse = await fetch('https://dummyjson.com/users')
+            const finalData = await apiResponse.json()
+            
+            if(finalData){
+                setApiData(finalData.users.map((user) => `${user.lastName}` + ' ' + `${user.lastName}` + ' ' +  `${user.age}`));
+
+                
+            }
+
+            setLoading(false)
+        }
+
+        getDataFromApi()
+    }, [])
+
+    if(loading){
+        return(
+            <ActivityIndicator color={'red'} size="large"/>
+        )
+    }
+
+    return(
+        <View>
+            <Text>Api Data</Text>
+            <View>
+                <FlatList data={apiData} renderItem={(itemData) => <Text>{itemData.item}</Text>}/>
+            </View>
+        </View>
+    )
+}
